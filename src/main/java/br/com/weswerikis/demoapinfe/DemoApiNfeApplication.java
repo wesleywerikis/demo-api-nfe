472cd8b2788a658cb8ec7fdce5d25f09f9010327
Wesley Werikis <wesleywerikis@outlook.com>
2024-04-24 17:39:20 -0300

    config[start project]: created project

---
+++ src/main/java/br/com/weswerikis/demoapinfe/DemoApiNfeApplication.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis.demoapinfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiNfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiNfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weswerikis/demoapinfe/DemoApiNfeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weswerikis.demoapinfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiNfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
